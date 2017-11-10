0eb9731555a313bc2837f05d673a46e01e2511bd
Josh Beam <joshua.a.beam@gmail.com>
2017-11-09 16:35:24 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# `library`
+
+A library that consists solely of semantically meaningful functions that do nothing important
+
+### TODO
+
+- add tests
+- add more functions
---
+++ index.js
@@ -0,0 +1,59 @@
+module.exports = {
+  /*
+    Example:
+    To get the number 12, you could do --
+      +([library.number.one(), library.number.two()].join('')) // => 12
+  */
+  number: {
+    zero,
+    one,
+    two,
+    three,
+    four,
+    five,
+    six,
+    seven,
+    eight,
+    nine
+  }
+}
+
+function zero() {
+  return 0
+}
+
+function one() {
+  return 1
+}
+
+function two() {
+  return 2
+}
+
+function three() {
+  return 3
+}
+
+function four() {
+  return 4
+}
+
+function five() {
+  return 5
+}
+
+function six() {
+  return 6
+}
+
+function seven() {
+  return 7
+}
+
+function eight() {
+  return 8
+}
+
+function nine() {
+  return 9
+}
